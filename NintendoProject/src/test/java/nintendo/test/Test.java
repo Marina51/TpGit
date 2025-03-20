525c52a6048cb922811072640e179278d1356d07
Marina Aubry <aubry.marina20@gmail.com>
2025-03-20 17:50:38 +0100

    Objets jeux dans main

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -4,7 +4,16 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
+		Console xbox = new Console ("xbox");
+		Console wii = new Console("wii");
+	
+		
+		Jeu zelda = new Jeu("Zelda",wii);
+		Jeu mario = new Jeu("Mario",xbox);
+		Jeu smashbross = new Jeu("Smash Bross",wii);
+		Jeu Fornite = new Jeu("Fortnite",xbox);
+		Jeu pokemon = new Jeu("Pokemon",wii);
 	}
 
 }
