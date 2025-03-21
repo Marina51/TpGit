9e3a97c545cb0df7253b872c3d115ea5a55e34bc
amir97600 <amir97600@github.com>
2025-03-21 10:13:56 +0100

    abstract console

--- NintendoProject/src/main/java/nintendo/model/Salon.java
+++ NintendoProject/src/main/java/nintendo/model/Salon.java
@@ -1,11 +1,15 @@
 package nintendo.model;
 
+import java.time.LocalDate;
+
 public class Salon extends Console{
 
-	public Salon(String nom) {
-		super(nom);
+	public Salon(String nom, double prix, LocalDate dateSortie) {
+		super(nom, prix, dateSortie);
 		// TODO Auto-generated constructor stub
 	}
+
+	
 
 	
 
