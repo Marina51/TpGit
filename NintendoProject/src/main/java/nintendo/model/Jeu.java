de4f47f4f1e1109d5b35ab60ae9dafa6134ce8ea
amir97600 <amir97600@github.com>
2025-03-21 09:37:31 +0100

    add Boutique to Jeu

--- NintendoProject/src/main/java/nintendo/model/Jeu.java
+++ NintendoProject/src/main/java/nintendo/model/Jeu.java
@@ -4,12 +4,13 @@ public class Jeu {
 
 	private String titre;
 	private Console console;
+	private Boutique boutique;
 	
-	
-	
-	public Jeu(String titre, Console console) {
+
+	public Jeu(String titre, Console console, Boutique boutique) {
 		this.titre = titre;
 		this.console = console;
+		this.boutique = boutique;
 	}
 
 
@@ -35,12 +36,22 @@ public class Jeu {
 	public void setConsole(Console console) {
 		this.console = console;
 	}
+	
+
+	public Boutique getBoutique() {
+		return boutique;
+	}
+
+
+	public void setBoutique(Boutique boutique) {
+		this.boutique = boutique;
+	}
 
 
 
 	@Override
 	public String toString() {
-		return "Jeu [titre=" + titre + ", console=" + console + "]";
+		return "Jeu [titre=" + titre + ", console=" + console + ", boutique=" + boutique + "]";
 	}
 	
 	
